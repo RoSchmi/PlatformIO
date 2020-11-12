28dbc90f92e476c9542e4297bd7b8b13647bf79b
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2020-11-13 00:06:59 +0100

    Added Wio_Terminal_Get_GitHub_Example

---
+++ Proj/Wio_Terminal_Get_GitHub_Example/include/config.h
@@ -0,0 +1,7 @@
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+// This file is for not secret user specific configurations
+
+#endif // _CONFIG_H
---
+++ Proj/Wio_Terminal_Get_GitHub_Example/include/config_secret_template.h
@@ -0,0 +1,10 @@
+#ifndef _CONFIG_SECRET_H
+#define _CONFIG_SECRET_H
+
+// Rename config_secret_template.h into config_secret.h to activate the content
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID            "YourSSID"
+#define IOT_CONFIG_WIFI_PASSWORD        "YourWiFiKey"
+
+#endif // _CONFIG_SECRET_H
