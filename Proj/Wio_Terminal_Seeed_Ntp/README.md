26eeb26c8f8326e34406e4f08bd809da2778d81c
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2020-11-21 01:15:44 +0100

    Added Wio_Terminal_Seeed_Ntp

---
+++ Proj/Wio_Terminal_Seeed_Ntp/README.md
@@ -0,0 +1,3 @@
+### Wio_Terminal_Seeed_Ntp
+
+This is a modification of the Seeed NTP example. 
---
+++ Proj/Wio_Terminal_Seeed_Ntp/include/config.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+// This file is for not secret user specific configurations
+
+
+#endif // _CONFIG_H
